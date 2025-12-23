a4490e496e1fea76c69c2a20c23e1829878b6d91
einav9642-wq <einav9642@gmail.com>
2025-12-23 16:12:18 +0200

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import pandas as pd
 import os
-
+# הוספת הלוגו בראש העמוד
+# width=150 קובע את הגודל של הלוגו (תוכל לשנות לפי הצורך)
+st.image("logo.png", width=150)
 # הגדרת סיסמה
 PASSWORD = "123" # שנה לסיסמה שלך
 
@@ -88,3 +90,4 @@ if check_password():
     if st.sidebar.button("התנתק"):
         st.session_state["password_correct"] = False
         st.rerun()
+
